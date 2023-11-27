501f703076c1cc647d837f8cd00b2fa331fc008e
Craig B <craig@bonaccorsi.net>
2023-11-27 17:44:49 -0600

    tute1finish

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
+
 export default defineConfig({
-    site: "https://bonaccorsi.net"
+  site: "https://bonaccorsi.net",
+  integrations: [preact()]
 });
