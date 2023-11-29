bff2f6b0db2029b80febddff9d39f557f89b6d7b
Craig B <craig@bonaccorsi.net>
2023-11-29 17:47:01 -0600

    kidpicsadd

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -9,11 +9,17 @@ export default function Greeting({messages}) {
   const randomMessage = () => messages[(Math.floor(Math.random() * messages.length))];
 
   const [greeting, setGreeting] = useState(messages[0]);
+ 
+  //CREATED MY OWN DYNAMIC IMAGE VARIABLE!!!!
+  let kidpic = greeting.concat(".jpg");
+ 
 
   return (
+   
     <div>
-      <h3>{greeting}! Is one of my children!!</h3>
-      <button onClick={() => setGreeting(randomMessage())}>New Child</button>
+      <img src={kidpic} height="300"/>
+      <h3>{greeting} is one of my children!!</h3>
+      <button  onClick={() => setGreeting(randomMessage())}>New Child</button>
     </div>
   );
 }
