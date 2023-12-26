15870d83a427830ccf4424318dbc88ce944a3405
Craig B <craig@bonaccorsi.net>
2023-12-26 00:30:28 -0600

    ASBMEMOIRS

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -19,7 +19,7 @@ return (
     <div>
       <img src={kidpic} height="300"/>
       <h3>{greeting} is one of my children!!</h3>
-      <button class="kidbutt" onClick={() => setGreeting(randomMessage())}>New Child</button>
+      <button class="kidbutt" onClick={() => setGreeting(randomMessage())}>Click for New Child</button>
     </div>
   );
 
